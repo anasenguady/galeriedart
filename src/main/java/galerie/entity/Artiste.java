196b532d738d33a60bfb608d9f5d780597755eb1
anasenguady <74250696+anasenguady@users.noreply.github.com>
2021-03-14 15:14:50 +0100

    Update and rename Exposition.java to Artiste.java

---
+++ src/main/java/galerie/entity/Artiste.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galerie.entity;
+
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+
+/**
+ *
+ * @Macbook
+ */
+@Getter @Setter @NoArgsConstructor @RequiredArgsConstructor @ToString
+@Entity
+public class Artiste extends Personne{
+    
+    @Column(unique=true)
+    private String biographie;
+    
+    @OneToMany(mappedBy="artiste")
+    List<Tableau> tableaux = new LinkedList<>();
+    
+}
